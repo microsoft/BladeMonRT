3bf77f1f5b6de332aab47747ba22b9bfa4ca13c3
Nikhita Shanker <t-nshanker@microsoft.com>
2021-07-30 11:47:46 -0700

    use constructor

--- nodes/dummy_node_a/dummy_node_a_test.go
+++ nodes/dummy_node_a/dummy_node_a_test.go
@@ -7,10 +7,10 @@ import (
 )
 
 func TestDummyNodeA(t *testing.T) {
-	var dummyNode DummyNodeA = DummyNodeA{Node: nodes.Node{}, result: "dummy-node-result"}
+	var dummyNode *DummyNodeA = NewDummyNodeA()
 	var workflowContext *nodes.WorkflowContext = nodes.NewWorkflowContext()
-	dummyNode.Process(&dummyNode, workflowContext)
-	result := dummyNode.GetResult(&dummyNode, workflowContext)
+	dummyNode.Process(dummyNode, workflowContext)
+	result := dummyNode.GetResult(dummyNode, workflowContext)
 
 	assert.Equal(t, result, "dummy-node-result");
 }
