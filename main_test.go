aedd83fbdd624f331ca37df47c0dbee3d3597a19
nikhitashanker77 <87544471+nikhitashanker77@users.noreply.github.com>
2021-08-13 14:28:22 -0700

    Add unit tests for workflow scheduler (#10)
    * add tests
    
    * Add test for callback.
    
    * merge changes
    
    * Add end to end test.
    
    * tests
    
    * Add end to end test
    
    * Restructure to separate main test.
    
    * add test utils.
    
    * nits'
    
    * utils
    
    * utils
    
    * README
    
    * Fixes from code review.
    
    * Fixes from code review.
    
    * nits.
    
    * nit
    
    * Fix from code review.

---
+++ main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+	"log"
+	"io/ioutil"
+	"github.com/microsoft/BladeMonRT/test_configs"
+	"github.com/microsoft/BladeMonRT/logging"
+	gomock "github.com/golang/mock/gomock"
+)
+
+func TestMain(t *testing.T) {
+	// Assume
+	workflowsJson, err := ioutil.ReadFile(test_configs.TEST_WORKFLOW_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var workflowFactory WorkflowFactory = newWorkflowFactory(workflowsJson, NodeFactory{})
+
+	// Create the schedules JSON from a file with a single schedule.
+	schedulesJson, err := ioutil.ReadFile(test_configs.TEST_SCHEDULE_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockWorkflowScheduler := NewMockWorkflowSchedulerInterface(ctrl)
+
+	// Assert
+	// Set up assertion.
+	var cpuMonitoringExpectedEventQuery WinEventSubscribeQuery = WinEventSubscribeQuery{channel: "Application", query: "*[System[Provider[@Name='CpuSpeedMonitoring']]]"}
+	cpuMonitoringScheduleExpectedEventQueries := []WinEventSubscribeQuery{cpuMonitoringExpectedEventQuery}
+	mockWorkflowScheduler.EXPECT().addWinEventBasedSchedule(gomock.Any(), cpuMonitoringScheduleExpectedEventQueries)
+	var disk7ExpectedEventQuery WinEventSubscribeQuery = WinEventSubscribeQuery{channel: "System", query: "*[System[Provider[@Name='disk'] and EventID=7]]"}
+	var disk8ExpectedEventQuery WinEventSubscribeQuery = WinEventSubscribeQuery{channel: "System", query: "*[System[Provider[@Name='disk'] and EventID=8]]"}
+	diskScheduleExpectedEventQueries := []WinEventSubscribeQuery{disk7ExpectedEventQuery, disk8ExpectedEventQuery}
+	mockWorkflowScheduler.EXPECT().addWinEventBasedSchedule(gomock.Any(), diskScheduleExpectedEventQueries)
+
+	// Assume
+	var logger *log.Logger = logging.LoggerFactory{}.ConstructLogger("Main")
+	var mainObj *Main = &Main{WorkflowScheduler: mockWorkflowScheduler, logger: logger}
+	
+	// Action
+	mainObj.setupWorkflows(schedulesJson, workflowFactory)
+}
